b7209defee97f2664e4b08a014dc92d940aa8fda
Jeremy Beard <jeremy.beard96@gmail.com>
2018-03-18 21:28:32 -0500

    Add files via upload

---
+++ NumberFunApp/NumberFunApp/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NumberFunApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        private int number;
+
+        public void OnGet()
+        {
+        }
+
+        public void OnPost(){
+            string number = Request.Form["number"];
+
+        }
+    }
+}
